431f306085bdbe092554076800243ddea42f7cfd
abdo0mahallawy <74137547+mahallawy1@users.noreply.github.com>
2023-05-20 17:59:58 +0300

    Add files via upload

---
+++ Objects/gpio.d
@@ -0,0 +1,3 @@
+./objects/gpio.o: GPIO.c GPIO.h tm4c123gh6pm.h types.h \
+  bitwise_operation.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\time.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h
---
+++ Objects/svmfunctionsf16.d
@@ -0,0 +1,24 @@
+./objects/svmfunctionsf16.o: \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\SVMFunctionsF16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_linear_init_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\dsp\svm_functions_f16.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\arm_math_types_f16.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\arm_math_types.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\float.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\limits.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\arm_math_memory.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\dsp\none.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\dsp\utils.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Include\dsp\svm_defines.h \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_linear_predict_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_polynomial_init_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_polynomial_predict_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_rbf_init_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_rbf_predict_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_sigmoid_init_f16.c \
+  C:\Users\user\AppData\Local\Arm\Packs\ARM\CMSIS-DSP\1.14.4\Source\SVMFunctions\arm_svm_sigmoid_predict_f16.c
